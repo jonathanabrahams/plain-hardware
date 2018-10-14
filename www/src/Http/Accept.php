2f5a8c7b3de026d583ccac86bbf4709c5424d162
Jonathan Abrahams <jonathanabrahams@gmail.com>
2018-10-14 20:08:20 +0200

    Add unit tests

--- www/src/Http/Accept.php
+++ www/src/Http/Accept.php
@@ -4,18 +4,51 @@ class Accept
 {
     private $type;
     private $sub_type;
-    private $q=1;
+    /**
+     * Quality Values
+     *
+     * @var integer
+     */
+    private $q = 1;
+    /**
+     * Key/Value pair paraments
+     *
+     * @var array
+     */
+    private $parameters = array();
 
-    public function __construct($type, $sub_type, $q)
+    public function __construct($type, $sub_type, $q = 1, $parameters = [])
     {
         $this->type = $type;
         $this->sub_type = $sub_type;
+        $this->q = $q;
+        $this->parameters = $parameters;
     }
 
-    public static function create($accept)
+    public function accept()
     {
-        $items = explode(';', $accept);
-        list($type, $sub_type) = explode('/', array_shift($items));
-        return new self($type, $sub_type, $items);
+        return $this->type . " " . $this->sub_type . " " . $this->q . " " . json_encode($this->parameters);
+    }
+
+    public static function parse($accept)
+    {
+        // Parse media range
+        $found = \preg_match_all("#(?<media_range>(\*/\*)|([a-z]+/[a-z|\*]+))(?<tokens>.*)#i", strtolower($accept), $match);
+        $accepted = array_combine($match['media_range'], $match['tokens']);
+        if ($found) {
+            // Parse tokens
+            foreach ($accepted as $media_range => $tokens) {
+                $has_tokens = preg_match_all("#;\s*(?<keys>[a-z]+)\s*=\s*(?<values>[^;]+)#i", $tokens, $match_tokens);
+                if ($has_tokens) {
+                    $accepted[$media_range] = array_combine($match_tokens['keys'], array_map(function ($v) {
+                        return is_numeric($v) ? $v + 0 : trim($v);
+                    }, $match_tokens['values'])
+                    );
+                } else {
+                    $accepted[$media_range] = ["q" => 1];
+                }
+            }
+        }
+        return $found ? $accepted : false;
     }
 }
--- www/tests/Http/AcceptTest.php
+++ www/tests/Http/AcceptTest.php
@@ -3,28 +3,20 @@ use PHPUnit\Framework\TestCase;
 
 class AcceptTest extends TestCase
 {
-    public function standardHeader()
+    public function test_create_accept()
     {
-        return [
-            "standard" => ["text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"]
-        ];
-    }
-    /**
-     * @dataProvider standardHeader
-     */
-    public function test_create_accept($headers)
-    {
-        $sut =  \App\Http\AcceptHeader::create($headers);
-        $this->assertInstanceOf(\App\Http\AcceptHeader::class, $sut);
+        $sut =  new \App\Http\Accept("text", "html", 1);
+        $this->assertInstanceOf(\App\Http\Accept::class, $sut);
     }
 
-    /**
-     * @dataProvider standardHeader
-     */
-    public function test_list_headers($headers)
+    public function test_list_headers()
     {
-        $sut =  \App\Http\AcceptHeader::create($headers);
-        $accepts = $sut->accepts();
-        $this->assertEquals(["*/*"=>[]], $accepts[5]);
+        \App\Http\Accept::parse("*/*; q=1; level=3; version=1.2.3");
+        \App\Http\Accept::parse("*/html; q=1; level=3; version=1.2.3");
+        \App\Http\Accept::parse("text/*; q=1; level=3; version=1.2.3");
+        \App\Http\Accept::parse("text/html; q=1; level=3; version=1.2.3");
+        
+        \App\Http\Accept::parse("text/html");
+        \App\Http\Accept::parse("text/html;");
     }
 }
